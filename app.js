a8d2afdf8fbb80072b09dbff4d52687e791a6178
Pa Gutri <pagutri@gmail.com>
2022-04-27 09:11:03 -0500

    Add new route to return an object

--- app.js
+++ app.js
@@ -20,3 +20,10 @@ app.listen(port, () => {
 app.get('/launchx', (req, res) => {
     res.send('Bienvenidos a launchx')
 })
+
+// Regresando un objeto
+// localhost:3000/explorersInNode
+app.get('/explorersInNode', (req, res) => {
+    const explorer = {name: "Explorer", msg: "Hello"}
+    res.send(explorer)
+})
