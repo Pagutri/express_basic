b1fb17e2a708ff60228ed56d3b2274f0ec479636
Pa Gutri <pagutri@gmail.com>
2022-04-27 09:17:07 -0500

    Add new route to receive params from the url

--- app.js
+++ app.js
@@ -27,3 +27,10 @@ app.get('/explorersInNode', (req, res) => {
     const explorer = {name: "Explorer", msg: "Hello"}
     res.send(explorer)
 })
+
+// Query Params: Recibir parámetros por la url
+// http://localhost:3000/explorers/carlo
+// req.params = {"explorerName":"carlo"}
+app.get('/explorers/:explorerName', (req, res) => {
+    res.send(req.params)
+})
